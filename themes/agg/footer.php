fc846da7cc544157c2da4834d28b4fe1fb60b616
Joseph Delgado <josephdelgadoa@gmail.com>
2026-01-13 00:11:00 -0800

    Update footer credits to Powered by Nexa Sphere

--- themes/agg/footer.php
+++ themes/agg/footer.php
@@ -1,17 +1,7 @@
 <footer class="site-footer">
     <div class="container">
         <div class="site-info">
-            <a href="<?php echo esc_url(__('https://wordpress.org/', 'agg')); ?>">
-                <?php
-                /* translators: %s: CMS Name, i.e. WordPress. */
-                printf(esc_html__('Proudly powered by %s', 'agg'), 'WordPress');
-                ?>
-            </a>
-            <span class="sep"> | </span>
-            <?php
-            /* translators: 1: Theme name, 2: Theme author. */
-            printf(esc_html__('Theme: %1$s by %2$s.', 'agg'), 'AGG Minimalist', 'AGG Team');
-            ?>
+            <span>Powered by Nexa Sphere</span>
             <br>
             &copy;
             <?php echo date('Y'); ?>
